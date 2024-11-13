c609a2074b6a2303e9aa60a01bdd2dc10295cc13
Manohar Prajapati <65000572+manohar9982@users.noreply.github.com>
2024-11-13 13:18:57 +0530

    view, Button, Text

---
+++ App.jsx
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import { Button, Text,View  } from 'react-native';
+const App = () => {
+  return (
+    <View>
+      <Text style={{fontSize:30}}>REact native Mopbile APP Devloper</Text>
+      <Text style={{fontSize:30}}>Manohar</Text>
+      <Button title='Press Here'/>
+
+    </View>
+    
+  );
+};
+
+export default App;
